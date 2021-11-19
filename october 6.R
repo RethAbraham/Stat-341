051744f1dc3648260756de9dee90bf47ebb8ac2a
RethAbraham <89413606+RethAbraham@users.noreply.github.com>
2021-11-19 10:40:15 -0800

    Add files via upload

---
+++ october 6.R
@@ -0,0 +1,6 @@
+# Author: Reth Abraham, Date: 10/6/2021, Purpose: Calculate the probability distribution values
+
+# Calculate Continuous Uniform distribution probability 
+runif(10, min=1, max=3)
+
+
